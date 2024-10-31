0ae3e60b41c02313f9d402a5d887d12c817c1ca8
LiuWei <183074632@qq.com>
2024-10-31 18:45:16 +0800

    fix: remove repeat code

--- allied_vision_api/controller.py
+++ allied_vision_api/controller.py
@@ -1,12 +1,7 @@
 """接收客户端发来的指令然后操控相机模块."""
 import asyncio
-import datetime
 import json
-import logging
-import os
-import sys
 import threading
-from logging.handlers import TimedRotatingFileHandler
 
 from socket_cyg.socket_server_asyncio import CygSocketServerAsyncio
 
@@ -15,38 +10,12 @@ from allied_vision_api.camera_api import CameraApi
 
 class Controller:
     """Controller class."""
-    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(module)s:%(lineno)d - %(message)s"
 
     def __init__(self):
-        self._logger = logging.getLogger(f"{self.__module__}.{self.__class__.__name__}")
-        self._file_handler = None
-        self.set_log()
-
-        self.camera_common = CameraApi()
+        self.camera_api = CameraApi()
+        self._logger = self.camera_api.logger
         self.socket_server = CygSocketServerAsyncio()
         self.start_server_thread()
-
-    @property
-    def file_handler(self):
-        """保存日志的日志器."""
-        if self._file_handler is None:
-            log_dir = f"{os.getcwd()}/log"
-            os.makedirs(log_dir, exist_ok=True)
-            file_name = f"{log_dir}/{datetime.datetime.now().strftime('%Y-%m-%d')}_Controller.log"
-            self._file_handler = TimedRotatingFileHandler(
-                file_name, when="D", interval=1, backupCount=10, encoding="UTF-8"
-            )
-        return self._file_handler
-
-    def set_log(self):
-        """设置日志."""
-        self.file_handler.setFormatter(logging.Formatter(self.LOG_FORMAT))
-        self.file_handler.setLevel(logging.INFO)
-        self._logger.addHandler(self.file_handler)
-        if sys.version_info.minor == 11:
-            logging.basicConfig(level=logging.INFO, encoding="UTF-8", format=self.LOG_FORMAT)
-        else:
-            self._logger.setLevel(logging.INFO)
 
     def start_server_thread(self):
         """启动供下位机连接的socket服务, 指定处理客户端连接的处理器."""
@@ -71,7 +40,7 @@ class Controller:
         for command, info in data_dict.items():
             self._logger.info("%s 收到客户端指令: %s %s", "-" * 20, command, "-" * 20)
             self._logger.info("***指令包含的数据*** -> %s", info)
-            if hasattr(self.camera_common, command):
-                getattr(self.camera_common, command)(**info)
+            if hasattr(self.camera_api, command):
+                getattr(self.camera_api, command)(**info)
                 self._logger.info("%s 指令执行结束 %s", "-" * 20, "-" * 20)
         return "@_@"
